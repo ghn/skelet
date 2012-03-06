f5e0ae60316976b03738fb0f3890ecc77b7f0392
Germain <ghugon@gmail.com>
2012-03-06 11:40:40 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+require(["core/jquery.min",
+	"order!core/jquery.color",
+	"order!core/underscore.min",
+	"order!core/backbone.min",
+	"order!core/bootstrap.min",
+	"order!models/friend.class"], function(util) {
+	
+	$(function() {
+		
+		friends = new friendCollection;
+    });
+});
